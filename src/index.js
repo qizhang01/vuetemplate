3ce0720a6aecbaa0d9c306fe929c1990216d6d46
zhangqi <zhishui120@sina.com>
2020-11-18 10:32:51 +0800

    初始化提交

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import router from './router'
+import App from './App'
+import { Button, Table, TableColumn, Pagination } from 'element-ui'
+
+// import 'assets/css/app.styl'
+import 'assets/css/index.css'
+// 常用组件在这注册。即可实现按需加载，又不必每个页面调用Vue.use。
+Vue.use(Button).use(Table).use(TableColumn).use(Pagination);
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
