dccb9eed6b1a5f80b34203463e06927cac5ff3cc
zhangqi <zhishui120@sina.com>
2020-11-22 19:53:47 +0800

    update: 完成

--- test/specs/focusPoint.spec.js
+++ test/specs/focusPoint.spec.js
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/vue'
 import IkeaFocusPoint from '@/components/focusPoint'
 
+//由于单元测试比较简单 也是时间原因 只是写了一个例子 测试通过即可。
+
 describe('focusPoint', () => {
   it('renders when passed', () => {
     const denominator = 4.5,  numerator = 5, totalNumber=220
